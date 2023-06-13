3fd57df1cd6ce34e740721f065aca727f2fc79e8
uwubakadontkillme <136262433+uwubakadontkillme@users.noreply.github.com>
2023-06-13 12:51:22 +0200

    Update fdhadfiaodfgdfgdfgd.lua

--- fdhadfiaodfgdfgdfgd.lua
+++ fdhadfiaodfgdfgdfgd.lua
@@ -4,7 +4,7 @@ local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/nic
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 for i,v in pairs(HWIDTable) do
 	if v == HWID then
-		
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/uwubakadontkillme/dkdafafasfasdadamafaka/main/sdasdasasiudviausdva.lua", true))()
 		else
 		game.Players.LocalPlayer.player:Kick("sorry but you are not whitelisted :(")
 	end
