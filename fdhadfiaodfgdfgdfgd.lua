120db2d7c08ed78ba91b8382f56a120a873398be
mikusgszyp <118010017+mikusgszyp@users.noreply.github.com>
2023-06-13 13:24:37 +0200

    jhiulnhiubyhubkuhgjkbvhjguhbk

--- fdhadfiaodfgdfgdfgd.lua
+++ fdhadfiaodfgdfgdfgd.lua
@@ -3,19 +3,17 @@ wait(2)
 -- HWID Whitelist
 
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId();
-local WhitelistedHWIDs = {"8873BB6E-5893-41AA-AABB-BAE745C91391", "2B509E2B-559A-4D36-A28F-DD57A2C9D68C", "2136ud98d984-414f-frfr-HWDP-dad"}
+local HWID1 = "8873BB6E-5893-41AA-AABB-BAE745C91391"
+local HWID2 = "2B509E2B-559A-4D36-A28F-DD57A2C9D68C"
 local qNVAKkuwxNpqruLjSRHg = false
 
 function CheckHWID(hwidval)
-for _,whitelisted in pairs(WhitelistedHWIDs) do
- print(whitelisted)
- print(hwidval)
- if hwidval == whitelisted then
-     return true
- elseif hwidval ~= whitelisted then
-     return false
-       end
-    end
+	
+	if (hwidval == HWID1 or hwidval == HWID2) then
+		return true
+	else
+		return false
+	end
 end
 
 
